bfd03c75b1c8ee8c09bf1407d38fac12fe5cfac4
Ian Stanton <ian@astronomer.io>
2020-10-09 12:23:13 -0500

    Bump AWS module and fix aurora bug (#45)
    * Bump AWS module to 1.1.159
    
    - Related to https://github.com/astronomer/issues/issues/1913
    
    * Pin terraform image to v0.12.29
    
    - Avoids v0.13 incompatibilities
    
    * Bump astronomer version to 0.16.8

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "aws" {
   source  = "astronomer/astronomer-aws/aws"
-  version = "1.1.144"
+  version = "1.1.159"
   # source                        = "../terraform-aws-astronomer-aws"
   deployment_id                 = var.deployment_id
   admin_email                   = var.email
