bc935c6c8c609b685881736b3df02ed3be9526ea
Ian Stanton <ian@astronomer.io>
2021-01-07 19:03:35 -0600

    Update tag value in slack notification

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "aws" {
   source  = "astronomer/astronomer-aws/aws"
-  version = "2.0.103"
+  version = "2.0.108"
   # source                        = "../terraform-aws-astronomer-aws"
   deployment_id                 = var.deployment_id
   admin_email                   = var.email
