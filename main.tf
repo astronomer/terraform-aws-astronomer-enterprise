b4ecb5b903bce91b4ce8900703f455ffd49b0fe5
Ian Stanton <ian@astronomer.io>
2021-01-21 20:06:23 -0600

    Bump astronomer-aws module to 2.0.114

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "aws" {
   source  = "astronomer/astronomer-aws/aws"
-  version = "2.0.108"
+  version = "2.0.114"
   # source                        = "../terraform-aws-astronomer-aws"
   deployment_id                 = var.deployment_id
   admin_email                   = var.email
