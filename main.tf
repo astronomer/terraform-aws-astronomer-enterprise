ec3f25f60cafbb7a9cf2b085f47a0e05460931de
Ian Stanton <ian@astronomer.io>
2021-02-22 07:23:53 -0500

    Bump aws module

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "aws" {
   source  = "astronomer/astronomer-aws/aws"
-  version = "2.0.114"
+  version = "2.0.122"
   # source                        = "../terraform-aws-astronomer-aws"
   deployment_id                 = var.deployment_id
   admin_email                   = var.email
