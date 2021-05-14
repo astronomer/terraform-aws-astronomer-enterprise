0fe3f93ebd66cfcdf181f3f88b3ed513831c7caf
Ian Stanton <ian@astronomer.io>
2021-05-14 10:43:48 -0400

    Update aws module and astronomer version

--- variables.tf
+++ variables.tf
@@ -100,7 +100,7 @@ variable "ten_dot_what_cidr" {
 
 variable "astronomer_version" {
   description = "Version of the Astronomer platform installed"
-  default     = "0.23.12"
+  default     = "0.25.1"
   type        = string
 }
 
